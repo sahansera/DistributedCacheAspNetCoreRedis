39e67315700be851d641f803ba557aeee67b3368
Sahan Serasinghe <sahan.serasinghe@gmail.com>
2022-08-22 19:26:13 +0930

    Migrated to .NET 6 🎉

--- DistributedCache/Program.cs
+++ DistributedCache/Program.cs
@@ -1,26 +1,43 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using DistributedCache.Infrastructure;
+using DistributedCache.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace DistributedCache
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddHttpClient();
+
+builder.Services.AddStackExchangeRedisCache(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    options.Configuration = builder.Configuration.GetSection("Redis")["ConnectionString"];
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<IUserService, CachedUserService>();
+builder.Services.AddScoped<ICacheUserService, CacheUserService>();
+builder.Services.AddScoped<ICacheProvider, CacheProvider>();
+builder.Services.AddScoped<IHttpClient, HttpClient>();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
